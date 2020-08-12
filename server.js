7bd683c12af5556f74d874d66651d8a1b8f71209
liuse3 <sedrick.liu@oocl.com>
2020-08-12 16:07:52 +0800

    index page

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require('http')
 var fs = require('fs')
 var path = require('path')
 var mime = require('mime')
+var chatServer = require('./lib/chat_server')
+
 
 var cache = {}
 
@@ -14,6 +16,44 @@ function send404(response){
 
 // file content response.
 function sendFile(response, filePath, fileContents){
-    response.writeHead(200,{'content-Type':mime.lookup(path.basename(fileContents))})
+    response.writeHead(200,{'content-Type':mime.lookup(path.basename(filePath))})
     response.end(fileContents)
+}
+
+function serveStatic(response,cache,absPath){
+    if(cache[absPath]){
+        sendFile(response,absPath,cache[absPath])
+    }else{
+        fs.exists(absPath,function(exists){
+            if(exists){
+                fs.readFile(absPath,function(err,data){
+                    if(err){
+                        return send404(response)
+                    }else{
+                        cache[absPath] = data
+                        sendFile(response,absPath,data)
+                    }
+                })
+            }
+        })
+    }
+}
+try{
+    var server = http.createServer(function(request,response){
+        var filePath = false
+        if(request.url == '/'){
+            filePath = 'public/index.html'
+        }else{
+            filePath = 'public' + request.url
+        }
+        var absPath = './' + filePath
+        serveStatic(response,cache,absPath)
+    })
+    
+    server.listen(3000,function(){
+        console.log('Server listening on port 3000.')
+    })
+    // chatServer.listen(server)
+}catch(e){
+    console.log(e)
 }
